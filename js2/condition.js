69dd682f77573967ed960babc3898f0a4e0d6396
nashath123456 <151601113+nashath123456@users.noreply.github.com>
2024-05-28 12:18:32 +0530

    Add files via upload

---
+++ js2/condition.js
@@ -0,0 +1,10 @@
+if(5<10)
+    {
+        console.log('5 is lesser');
+    }
+    else
+    {
+        console.log('5 is greater');
+    }
+
+  (5<10)?console.log("5 is lesser"):console.log("5 is greater");
---
+++ js2/loop.js
@@ -0,0 +1,70 @@
+// for(let i =1;i<=10;i++){
+//     console.log(i)
+// }
+
+
+// let time =20;
+// if(time>=1 && time<=11){
+//     console.log("good morning")
+// }
+// else if(time>=12 && time<=16){
+//     console.log("good afternoon")
+// }
+// else if (time>=16 && time<=19){
+//     console.log("good evening")
+// }
+// else 
+//     {
+//         console.log("good night")
+//     }
+
+
+// let number = 13;
+// if(number %2 == 0){
+///     console.log(`${number}is even`)
+// }
+// else{
+//     console.log(`${number}is odd`)
+// }
+
+// let fruits=["apple","banana","graps","pineapple"];
+// for(let i=1; i<fruits.length;i++){
+//     console.log(fruits[i]);
+// }
+
+// const number =[1,2,3,4,5];
+// var sum=0;
+// for (let i=0;i<number.length;i++){
+//     sum =sum+number[i]
+
+// }
+//      console.log(sum);
+
+
+//print multiplication table 1-5m
+
+// for(let i =1; i<=5; i++){
+//     for (let j =1; j<=5;j++){
+//         let p = `${i}*${j}=${(i*j)}`
+//         console.log(p)
+
+//     }
+// }
+// let p= [1,2,3,4,5,6,7,8,9,10];
+// for (let i =0; i< p.length; i++){
+//     if(p[i] %2 ==0 ){
+//         console.log(`${p[i]}is even number`)
+//     }
+
+//     else {
+//         console.log(`${p[i]}is odd number`)
+//     }
+// }
+
+        
+
+    
+
+
+
+
